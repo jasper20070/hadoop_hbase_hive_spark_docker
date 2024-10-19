68aad19e550ad34911dbc8f79f2b875b2fea2f4e
jasper20070 <jasper20070@gmail.com>
2024-10-19 13:46:49 +0200

    Minor adjustments to bonus assignment python code and added a simple Pytest

--- Bonus/Bonus_assignment.py
+++ Bonus/Bonus_assignment.py
@@ -8,39 +8,60 @@ fake = Faker()
 using a for loop. Also adding a timestamp in Event_Date_Records so it is
 suitable for Kafka"""
 
-data = []
 
-for i in range(10000):
-    record = {
-        'PERSONAL_NUMBER_ID': i + 1,
-        'FULL_NAME': fake.name(),
-        'BIRTH_DATE': fake.date_of_birth(minimum_age=18, maximum_age=90).isoformat(),
-        'SSN': fake.ssn(),
-        'EVENT_DATE_RECORD': fake.iso8601(),
-        'PERSONAL_ADDRESS_STREET': fake.street_address(),
-        'PERSONAL_ADDRESS_CITY': fake.city(),
-        'PERSONAL_ADDRESS_STATE': fake.state(),
-        'PERSONAL_ADDRESS_COUNTRY': fake.country(),
-        'PERSONAL_ADDRESS_ZIP_CODE': fake.zipcode()
-    }
+def generate_data(num_records):
+    data = []
 
-    data.append(record)
+    for i in range(num_records):
+        record = {
+            'PERSONAL_NUMBER_ID': i + 1,
+            'FULL_NAME': fake.name(),
+            'BIRTH_DATE': fake.date_of_birth(minimum_age=18, maximum_age=90).isoformat(),
+            'SSN': fake.ssn(),
+            'EVENT_DATE_RECORD': fake.iso8601(),
+            'PERSONAL_ADDRESS_STREET': fake.street_address(),
+            'PERSONAL_ADDRESS_CITY': fake.city(),
+            'PERSONAL_ADDRESS_STATE': fake.state(),
+            'PERSONAL_ADDRESS_COUNTRY': fake.country(),
+            'PERSONAL_ADDRESS_ZIP_CODE': fake.zipcode()
+        }
 
-df = pd.DataFrame(data)
+        data.append(record)
+
+    df = pd.DataFrame(data)
+
+    return df
+
 
 """Making sure that the data is suitable for Kafka """
 
+
 # Verify serialization
-sample_record = df.iloc[0].to_dict()
-json_record = json.dumps(sample_record)
+def validate(df):
+    sample_record = df.iloc[0].to_dict()
+    json_record = json.dumps(sample_record)
 
-print("Sample Kafka Message (in JSON format):")
-print(json_record)
+    print("Sample Kafka Message (in JSON format):")
+    print(json_record)
 
-# Making sure message is under 1MB
-message_size = len(json_record.encode('utf-8'))
-print(f"\nSize of the message in bytes: {message_size} bytes")
+    # Making sure message is under 1MB
+    message_size = len(json_record.encode('utf-8'))
+    print(f"\nSize of the message in bytes: {message_size} bytes")
+
+    return
 
 """Exporting data to a JSON file"""
 
-df.to_json('personal_data.json', orient='records', lines=True)
+
+def to_json(df):
+    df.to_json('personal_data.json', orient='records', lines=True)
+
+    return
+
+
+df = generate_data(10000)
+
+validate(df)
+
+to_json(df)
+
